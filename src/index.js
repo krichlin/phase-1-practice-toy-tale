49a9f02bf51de227b7f8dd183d3c886380cf544d
Ken Richlin <krichlin@gmail.com>
2024-04-09 11:58:02 -0400

    part 2 working

--- src/index.js
+++ src/index.js
@@ -19,22 +19,22 @@ fetch('http://localhost:3000/toys')
   .then(data => renderImages(data))
   .catch(error => console.error(error))
 
-const target = document.getElementById('toy-collection')
+const toyCollection = document.getElementById('toy-collection')
 
 function renderImages(toyArray) {
     toyArray.forEach((toy) => {
     const toyCard = document.createElement('div')
-    toyCard.classList = ('card')
+    toyCard.classList = 'card'
     // console.log(toy)
     toyCard.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img src ='${toy.image}' class='toy-avatar' />
-    <p>${toy.likes}</p>
-    <button class ='like=btn' id='[toy_id]'>❤️</button>`
-    target.appendChild(toyCard)
+        <h2>${toy.name}</h2>
+        <img src ='${toy.image}' class='toy-avatar' />
+        <p>${toy.likes}</p>
+        <button class ='like=btn' id=${toy.id}'>❤️</button>
+        `
+    toyCollection.appendChild(toyCard)
     // console.log(target)
-    
-  })
+    })
 }
 
 const newToyForm = document.querySelector('.add-toy-form')
